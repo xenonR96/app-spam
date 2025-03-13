395f04cdad0d3b2020652d79524186b9a720a3c3
jesus-gp96 <75471949+jesus-gp96@users.noreply.github.com>
2025-03-13 13:38:08 +0100

    try update for render

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 import pandas as pd
 import re
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static", static_url_path="/")
 CORS(app)  # Enable CORS for frontend-backend communication
 
 
